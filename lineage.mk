10244f963561ae280f761a29009f9c2733d68954
Charles-IV <Charles-IV@users.noreply.github.com>
2017-07-24 10:30:12 +0100

    lineage.mk: 5010 -> 4034

--- lineage.mk
+++ lineage.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := 5010D
+PRODUCT_RELEASE_NAME := Alcatel Pixi 4 (4)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/TCL/5010D/device_5010D.mk)
+$(call inherit-product, device/tcl/4034X/device_4034X.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := 5010D
-PRODUCT_NAME := lineage_5010D
-PRODUCT_BRAND := TCL
-PRODUCT_MODEL := 5010D
+PRODUCT_DEVICE := 4034X
+PRODUCT_NAME := lineage_4034X
+PRODUCT_BRAND := Alcatel
+PRODUCT_MODEL := Alcatel Pixi 4 (4)
 PRODUCT_MANUFACTURER := TCL
 
